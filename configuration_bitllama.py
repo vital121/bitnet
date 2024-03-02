0d3e53eb97b984186f919b33db20a2bb636e6eff
vital121 <123862035+vital121@users.noreply.github.com>
2024-03-02 14:55:02 +0000

    Update configuration_bitllama.py

--- configuration_bitllama.py
+++ configuration_bitllama.py
@@ -26,80 +26,38 @@ logger = logging.get_logger(__name__)
 
 BITLLAMA_PRETRAINED_CONFIG_ARCHIVE_MAP = {}
 
-
 class BitLlamaConfig(PretrainedConfig):
-    r"""
-    This is the configuration class to store the configuration of a [`BitLlamaModel`]. It is used to instantiate a BitLlama
-    model according to the specified arguments, defining the model architecture. Instantiating a configuration with the
-    defaults will yield a similar configuration to that of Llama-7B.
-
-    Configuration objects inherit from [`PretrainedConfig`] and can be used to control the model outputs. Read the
-    documentation from [`PretrainedConfig`] for more information.
-
-
-    Args:
-        vocab_size (`int`, *optional*, defaults to 32000):
-            Vocabulary size of the LLaMA model. Defines the number of different tokens that can be represented by the
-            `inputs_ids` passed when calling [`LlamaModel`]
-        hidden_size (`int`, *optional*, defaults to 4096):
-            Dimension of the hidden representations.
-        intermediate_size (`int`, *optional*, defaults to 11008):
-            Dimension of the MLP representations.
-        num_hidden_layers (`int`, *optional*, defaults to 32):
-            Number of hidden layers in the Transformer decoder.
-        num_attention_heads (`int`, *optional*, defaults to 32):
-            Number of attention heads for each attention layer in the Transformer decoder.
-        num_key_value_heads (`int`, *optional*):
-            This is the number of key_value heads that should be used to implement Grouped Query Attention. If
-            `num_key_value_heads=num_attention_heads`, the model will use Multi Head Attention (MHA), if
-            `num_key_value_heads=1 the model will use Multi Query Attention (MQA) otherwise GQA is used. When
-            converting a multi-head checkpoint to a GQA checkpoint, each group key and value head should be constructed
-            by meanpooling all the original heads within that group. For more details checkout [this
-            paper](https://arxiv.org/pdf/2305.13245.pdf). If it is not specified, will default to
-            `num_attention_heads`.
-        hidden_act (`str` or `function`, *optional*, defaults to `"silu"`):
-            The non-linear activation function (function or string) in the decoder.
-        max_position_embeddings (`int`, *optional*, defaults to 2048):
-            The maximum sequence length that this model might ever be used with. Llama 1 supports up to 2048 tokens,
-            Llama 2 up to 4096, CodeLlama up to 16384.
-        initializer_range (`float`, *optional*, defaults to 0.02):
-            The standard deviation of the truncated_normal_initializer for initializing all weight matrices.
-        rms_norm_eps (`float`, *optional*, defaults to 1e-06):
-            The epsilon used by the rms normalization layers.
-        use_cache (`bool`, *optional*, defaults to `True`):
-            Whether or not the model should return the last key/values attentions (not used by all models). Only
-            relevant if `config.is_decoder=True`.
-        pad_token_id (`int`, *optional*):
-            Padding token id.
-        bos_token_id (`int`, *optional*, defaults to 1):
-            Beginning of stream token id.
-        eos_token_id (`int`, *optional*, defaults to 2):
-            End of stream token id.
-        pretraining_tp (`int`, *optional*, defaults to 1):
-            Experimental feature. Tensor parallelism rank used during pretraining. Please refer to [this
-            document](https://huggingface.co/docs/transformers/parallelism) to understand more about it. This value is
-            necessary to ensure exact reproducibility of the pretraining results. Please refer to [this
-            issue](https://github.com/pytorch/pytorch/issues/76232).
-        tie_word_embeddings (`bool`, *optional*, defaults to `False`):
-            Whether to tie weight embeddings
-        rope_theta (`float`, *optional*, defaults to 10000.0):
-            The base period of the RoPE embeddings.
-        rope_scaling (`Dict`, *optional*):
-            Dictionary containing the scaling configuration for the RoPE embeddings. Currently supports two scaling
-            strategies: linear and dynamic. Their scaling factor must be a float greater than 1. The expected format is
-            `{"type": strategy name, "factor": scaling factor}`. When using this flag, don't update
-            `max_position_embeddings` to the expected new maximum. See the following thread for more information on how
-            these scaling strategies behave:
-            https://www.reddit.com/r/LocalLLaMA/comments/14mrgpr/dynamically_scaled_rope_further_increases/. This is an
-            experimental feature, subject to breaking API changes in future versions.
-        attention_bias (`bool`, defaults to `False`, *optional*, defaults to `False`):
-            Whether to use a bias in the query, key, value and output projection layers during self-attention.
-        attention_dropout (`float`, *optional*, defaults to 0.0):
-            The dropout ratio for the attention probabilities.
-        layer_repeat (`int`, *optional*, defaults to 1):
-            The number of times each layer is repeated.
     """
+    Configuration class for BitLlamaModel.
 
+    This class stores the configuration of a BitLlamaModel and is used to instantiate the model
+    with specific arguments, defining its architecture. The defaults are similar to the Llama-7B model configuration.
+
+    Inherits from PretrainedConfig with additional model-specific parameters for controlling model outputs.
+
+    Parameters:
+        vocab_size (int, optional): Vocabulary size, default is 32000.
+        hidden_size (int, optional): Dimension of hidden layers, default is 4096.
+        intermediate_size (int, optional): Size of the intermediate layer in the feedforward network, default is 11008.
+        num_hidden_layers (int, optional): Number of hidden layers in the Transformer model, default is 32.
+        num_attention_heads (int, optional): Number of attention heads, default is 32.
+        num_key_value_heads (int, optional): Number of key/value heads for Grouped Query Attention, defaults to num_attention_heads.
+        hidden_act (str or callable, optional): Activation function, default is "silu".
+        max_position_embeddings (int, optional): Maximum sequence length, default is 2048.
+        initializer_range (float, optional): Standard deviation of the initializer, default is 0.02.
+        rms_norm_eps (float, optional): Epsilon for RMS normalization, default is 1e-6.
+        use_cache (bool, optional): If True, enables caching for faster inference, default is True.
+        pad_token_id (int, optional): ID for the padding token.
+        bos_token_id (int, optional): Beginning of stream token ID, default is 1.
+        eos_token_id (int, optional): End of stream token ID, default is 2.
+        pretraining_tp (int, optional): Tensor parallelism rank for pretraining, default is 1.
+        tie_word_embeddings (bool, optional): If True, ties the word embeddings, default is False.
+        rope_theta (float, optional): Base period for RoPE embeddings, default is 10000.0.
+        rope_scaling (dict, optional): Configuration for RoPE embeddings scaling.
+        attention_bias (bool, optional): If True, adds bias to attention layers, default is False.
+        attention_dropout (float, optional): Dropout rate for attention probabilities, default is 0.0.
+        layer_repeat (int, optional): Number of times each layer is repeated, default is 1.
+    """
     model_type = "llama"
     keys_to_ignore_at_inference = ["past_key_values"]
 
@@ -128,30 +86,11 @@ class BitLlamaConfig(PretrainedConfig):
         layer_repeat=1,
         **kwargs,
     ):
-        self.vocab_size = vocab_size
-        self.max_position_embeddings = max_position_embeddings
-        self.hidden_size = hidden_size
-        self.intermediate_size = intermediate_size
-        self.num_hidden_layers = num_hidden_layers
-        self.num_attention_heads = num_attention_heads
-
-        # for backward compatibility
+        # Setting num_key_value_heads to num_attention_heads if not specified
         if num_key_value_heads is None:
             num_key_value_heads = num_attention_heads
-
-        self.num_key_value_heads = num_key_value_heads
-        self.hidden_act = hidden_act
-        self.initializer_range = initializer_range
-        self.rms_norm_eps = rms_norm_eps
-        self.pretraining_tp = pretraining_tp
-        self.use_cache = use_cache
-        self.rope_theta = rope_theta
-        self.rope_scaling = rope_scaling
-        self._rope_scaling_validation()
-        self.attention_bias = attention_bias
-        self.attention_dropout = attention_dropout
-        self.layer_repeat = layer_repeat
-
+        
+        # Initialize parent class with all provided arguments
         super().__init__(
             pad_token_id=pad_token_id,
             bos_token_id=bos_token_id,
@@ -160,29 +99,45 @@ class BitLlamaConfig(PretrainedConfig):
             **kwargs,
         )
 
-    def _rope_scaling_validation(self):
-        """
-        Validate the `rope_scaling` configuration.
-        """
-        if self.rope_scaling is None:
-            return
+        # Model-specific configurations
+        self.vocab_size = vocab_size
+        self.max_position_embeddings = max_position_embeddings
+        self.hidden_size = hidden_size
+        self.intermediate_size = intermediate_size
+        self.num_hidden_layers = num_hidden_layers
+        self.num_attention_heads = num_attention_heads
+        self.num_key_value_heads = num_key_value_heads
+        self.hidden_act = hidden_act
+        self.initializer_range = initializer_range
+        self.rms_norm_eps = rms_norm_eps
+        self.pretraining_tp = pretraining_tp
+        self.use_cache = use_cache
+        self.rope_theta = rope_theta
+        self.rope_scaling = self.validate_rope_scaling(rope_scaling)
+        self.attention_bias = attention_bias
+        self.attention_dropout = attention_dropout
+        self.layer_repeat = layer_repeat
 
-        if not isinstance(self.rope_scaling, dict) or len(self.rope_scaling) != 2:
-            raise ValueError(
-                "`rope_scaling` must be a dictionary with with two fields, `type` and `factor`, "
-                f"got {self.rope_scaling}"
-            )
-        rope_scaling_type = self.rope_scaling.get("type", None)
-        rope_scaling_factor = self.rope_scaling.get("factor", None)
-        if rope_scaling_type is None or rope_scaling_type not in ["linear", "dynamic"]:
-            raise ValueError(
-                f"`rope_scaling`'s type field must be one of ['linear', 'dynamic'], got {rope_scaling_type}"
-            )
-        if (
-            rope_scaling_factor is None
-            or not isinstance(rope_scaling_factor, float)
-            or rope_scaling_factor <= 1.0
-        ):
-            raise ValueError(
-                f"`rope_scaling`'s factor field must be a float > 1, got {rope_scaling_factor}"
-            )
+    def validate_rope_scaling(self, rope_scaling):
+        """
+        Validates the `rope_scaling` configuration.
+
+        Parameters:
+            rope_scaling (dict): Configuration for RoPE scaling.
+
+        Returns:
+            dict: Validated RoPE scaling configuration.
+        """
+        if rope_scaling is None:
+            return None
+        
+        if not isinstance(rope_scaling, dict) or 'type' not in rope_scaling or 'factor' not in rope_scaling:
+            raise ValueError("`rope_scaling` must be a dictionary with 'type' and 'factor' keys.")
+        
+        if rope_scaling['type'] not in ['linear', 'dynamic']:
+            raise ValueError("`rope_scaling['type']` must be either 'linear' or 'dynamic'.")
+        
+        if not isinstance(rope_scaling['factor'], float) or rope_scaling['factor'] <= 1.0:
+            raise ValueError("`rope_scaling['factor']` must be a float greater than 1.")
+        
+        return rope_scaling
